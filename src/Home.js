7b84f5b8e7df031d896435f21bf1d629e1b6abb7
Louay kharouf <louay.khrouf@insat.ucar.tn>
2022-11-30 00:32:47 +0100

    forms

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,46 @@
 import logo from './logo.svg';
 import Navbar from './Navbar';
+import courses from './Courses.png';
 function Home(){
     return(
         <div className="Home">
         
             <Navbar/>
             <section id="intro">
-            <div data-aos="fade-left">
+      <div class="welcome" data-aos="fade-right">
                 
-                <h1>
+                <h1 class="typewriter">
                 Welcome to Test-Me plateform </h1>
                 <h2>
                 Improve your skills and assign your technical tests 
-                </h2>
+                </h2>  
             </div>
+            <a href="#" class="btn-get-started">Get Started</a>
             </section>
+          <section id="services">
+            <h1 class="About">About Us</h1>
+       <p class="aboutdesc">Test Me is just a plateform that give to developpers the chance to practice some of their favorite skills 
+         and gives to recruiter a secure space to propose some technical tests </p>  
+         <h1 class="About">Our Services</h1>
+         <div class="box1">
+          <img class="courses" src={require('./Courses.png')}></img>
           
+          <p class="TC">Training Courses</p>
+          <p class="TT">Training Tests</p>
+         </div>
+         <div class="box2">
+         <img class="courses" src={require('./OnlineTest.png')}></img>
+          
+          <p class="TC">Technical Tests</p>
+          <p class="TT">Skill Tests</p>
+</div>
+<div class="box3">
+<img class="courses" src={require('./Fraud.png')}></img>
+          
+          <p class="TC">Fraud Detection</p>
+          
+</div>
+          </section>
       </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -18,14 +18,88 @@ function Navbar(){
         <a class="nav-link" href="#">Technical Test</a>
         
         <div class="left">
-        <a href="#" class="signup-btn">Sign Up</a>
-        <a href="#" class="signin-btn">Login</a>
+        <button type="button" class="btn signup-btn" data-bs-toggle="modal" data-bs-target="#registration">Sign Up</button>
+        <button type="button" class="btn signin-btn" data-bs-toggle="modal" data-bs-target="#logform">Login</button>
+
+
         
         </div>
       </div>
     </div>
   </div>
 </nav>
+<div class="modal fade" id="registration" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"> Welcome ! Register here </h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail2" class="form-label">Confirm E-mail</label>
+    <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp1"></input>
+    <div id="emailHelp1" class="form-text">Confirm The E-mail.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"></input>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword2" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2"></input>
+  </div>
+  <div class="mb-3">
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Sign Up as </option>
+  <option value="1">Recruiter</option>
+  <option value="2">Developer</option>
+
+</select>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign Up</button>
+</form>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="logform" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Welcome ! Login here</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label" >Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required></input>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" required></input>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Login</button>
+</form>
+      </div>
+      
+    </div>
+  </div>
+</div>
     </div>
     );
 }
